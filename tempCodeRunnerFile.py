a51dde5c461f441f8ab43ffaba75e3dc7087016f
Matt-W1566 <matthew.weinstein08@gmail.com>
2026-05-02 08:53:09 -0400

    2

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-            self.mode = new_mode
-            self.analyzer.set_mode(self.mode)
-            self.mode_badge.set_mode(self.mode, MODE_COLORS[self.mode])
-            self.caption_pill.set_text(MODE_CAPTIONS[self.mode])
-            self.caption_pill.set_color(MODE_COLORS[self.mode])
-            self.flash.trigger(self.mode, MODE_COLORS[self.mode])
